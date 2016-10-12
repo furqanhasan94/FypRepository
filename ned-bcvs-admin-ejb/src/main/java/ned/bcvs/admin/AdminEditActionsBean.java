6bb65f57b783574145e8d8f0283c162763feadc7
Furqan Hasan <furqan.hasan94@gmail.com>
2016-10-13 01:18:40 +0500

    Initial setup

---
+++ ned-bcvs-admin-ejb/src/main/java/ned/bcvs/admin/AdminEditActionsBean.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ned.bcvs.admin;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Local;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+import ned.bcvs.admin.cdi.qualifier.jdbcfypdb;
+import ned.bcvs.admin.dao.ConstituencyType;
+
+/**
+ *
+ * @author StackHouse
+ */
+@Stateful
+@Local(AdminEditActionsLocal.class)
+public class AdminEditActionsBean implements AdminEditActionsLocal {
+
+    @Inject
+    @jdbcfypdb
+    Connection conn;
+
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public List<ConstituencyType> getConstituencyTypes(String constituencyName) {
+        final List<ConstituencyType> constituencyTypes = new ArrayList<>();
+        try (PreparedStatement ps = conn.prepareStatement("Select * from ConstituencyType")) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                constituencyTypes.add(new ConstituencyType(rs.getInt("ConstituencyTypeId"), rs.getString("ConstituencyTypeName")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return constituencyTypes;
+    }
+
+}
---
+++ ned-bcvs-admin-war/src/main/java/ned/bcvs/admin/web/S1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ned.bcvs.admin.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ned.bcvs.admin.AdminEditActionsLocal;
+
+/**
+ *
+ * @author StackHouse
+ */
+@WebServlet(urlPatterns = "/s1")
+public class S1 extends HttpServlet {
+
+    @EJB
+    private AdminEditActionsLocal adminEditActionsLocal;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter w = resp.getWriter();
+        w.println(adminEditActionsLocal.getConstituencyTypes("ABC").toString());
+        w.close();
+    }
+}
